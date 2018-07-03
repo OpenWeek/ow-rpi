d90c87aaa99211e52be3cd10ee6f1bc0838bd524
QDelmelle <qdelmelle@vader.info.ucl.ac.be>
2018-07-03 17:07:58 +0200

    tests db reloaded

--- scripts/test_db.py
+++ scripts/test_db.py
@@ -3,11 +3,11 @@ import db_handler as db
 if __name__ == '__main__':
 
     start = 1534463400
-
+    """
     for t in range(7*24*3600/5):
         db.save_measure("temperature",start+t*300, 10+0.01*t)
         db.save_measure("pressure", start+t*300, 1000+0.1*t)
-
+    """
     db.get_measure_week("temperature")
     db.get_measure_week("pressure")
 
