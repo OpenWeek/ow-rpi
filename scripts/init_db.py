e48ac7f087ed04d5a884c0532e435ad3ff8a12c5
QDelmelle <qdelmelle@vader.info.ucl.ac.be>
2018-07-04 14:36:00 +0200

    bornes ajustées

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -2,7 +2,9 @@ import db_handler as db
 
 if __name__ == '__main__':
 
-    measures = ['temperature', 'pressure', 'humidity', 'luminosity', 'infrared', 'ultraviolet']
-
-    for measure in measures:
-        db.init_measure(measure)
+    db.init_measure('temperature', -273.15, 200)
+    db.init_measure('pressure', 800, 1300)
+    db.init_measure('luminosity', 0, 150000)
+    db.init_measure('humidity', 0, 5000)
+    db.init_measure('ultraviolet', 0, 5000)
+    db.init_measure('infrared', 0, 5000)
