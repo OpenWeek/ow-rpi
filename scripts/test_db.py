078ddb92c3e918174e2210aaf7e6d7b02b9573c4
QDelmelle <qdelmelle@vader.info.ucl.ac.be>
2018-07-04 10:28:13 +0200

    4

--- scripts/test_db.py
+++ scripts/test_db.py
@@ -3,14 +3,14 @@ import time
 
 if __name__ == '__main__':
 
-    interval = 24*3600
+    interval = 3600
     start = time.time() - interval
 
-    """
+
     for t in range(interval/300):
         db.save_measure("temperature", start+t*300, 10+0.01*t)
         db.save_measure("pressure", start+t*300, 1000+0.1*t)
-    """
+
 
     print(db.get_measure_from("temperature", interval))
     print(db.get_measure_from("pressure", interval))
