1160ccc3fe34090c5389b24fe788bf76cb7780a1
FrancoMaxime <franco.maxime@gmail.com>
2018-07-05 17:28:09 +0200

    ajout database alarm

---
+++ ow_rpi/alarm/db_alarm.py
@@ -0,0 +1,78 @@
+import 	sqlite3
+import time
+import yaml
+
+def get_config():
+	with open("ow_rpi/config/alarm_config.yaml", 'r') as stream:
+		try:
+			return yaml.safe_load(stream)
+		except yaml.YAMLError as exc:
+			print(exc)
+
+def get_database():
+	config = get_config()
+	conn = sqlite3.connect("ow_rpi/storage/"+config['database']['name'])
+	return conn
+
+def init_db():
+	conn = get_database()
+	c = conn.cursor()
+	c.execute("DROP TABLE IF EXISTS log")
+	c.execute("CREATE TABLE log (timestamp INT, measure TEXT, value REAL)" )
+	conn.commit()
+	conn.close()
+	
+def save_log(timestamp, measure, value):
+	conn = get_database()
+	c = conn.cursor()
+	c.execute("INSERT INTO log VALUES ('"+ str(timestamp) +"', '"+ str(measure) +"', '"+ str(value) +"' )" )
+	conn.commit()
+	conn.close()
+	
+def get_log_now(measure):
+    now = get_now()
+    return get_log_from(measure, now -300, now)
+
+def get_log_minute(measure):
+    return get_log_now(measure)
+
+def get_log_hour(measure):
+    now = get_now()
+    return get_log_from(measure, now -3600, now)
+
+def get_log_day(measure):
+    now = get_now()
+    return get_log_from(measure, now - (24*3600), now)
+
+def get_log_week(measure):
+    now = get_now()
+    return get_log_from(measure, now -(7*24*3600), now)
+
+def get_log_month(measure):
+    now = get_now()
+    return get_log_from(measure, now -(4*7*24*3600), now)
+    
+def get_log_from(measure, start, end):
+	conn = get_database()
+	c = conn.cursor()
+	print "SELECT * FROM log where measure = '"+ measure +"' AND '"+ str(start) +"' <= timestamp AND timestamp <= '" + str(end) + "'"
+	test = c.execute("SELECT * FROM log where measure = '"+ measure +"' AND "+ str(start) +" <= timestamp AND timestamp <= " + str(end) )
+	result = []
+	for i in test:	
+		result.append(i)
+	conn.close()
+	return result
+	
+def get_log_all(measure):
+	conn = get_database()
+	c = conn.cursor()
+	test = c.execute("SELECT * FROM log where measure = '"+ measure  + "'")
+	result = []
+	for i in test:
+		result.append(i)
+	conn.close()
+	return result	
+	
+def get_now():
+	return int(time.time())
+
---
+++ ow_rpi/alarm/init_database_alarm.py
@@ -0,0 +1,4 @@
+from ow_rpi.alarm.db_alarm import *
+
+if __name__ == '__main__':
+	init_db()
