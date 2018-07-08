9ac41f8458fb5302b6608419f73d311afb29d8e4
nrybowski <nicolas.rybowski@student.uclouvain.be>
2018-07-08 13:04:21 +0200

    Update license

--- ow_rpi/alarm/init_database_alarm.py
+++ ow_rpi/alarm/init_database_alarm.py
@@ -1,20 +1,21 @@
-# Openweek Raspberry pi's weather station
-# Copyright c 2018  Maxime Postaire, Lucas Ody, Maxime Franco,
-# Nicolas Rybowski, Benjamin De Cnuydt, Quentin Delmelle, Colin Evrard,
-# Antoine Vanderschueren.
+# Openweek Raspberry Pi's Weather Station
+# Copyright c 2018  
+# Benjamin De Cnuydt, Quentin Delmelle, Robin Descamps
+# Colin Evrard, Maxime Franco, Lucas Ody, 
+# Maxime Postaire, Nicolas Rybowski, Antoine Vanderschueren.
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from ow_rpi.alarm.db_alarm import *
 
